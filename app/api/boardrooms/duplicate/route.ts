0ef228c34cd175d390bda02c9d55d26b98823c26
Janith Madushan <janithmadu281@gmail.com>
2025-10-23 15:04:11 +0530

    Update

--- app/api/boardrooms/duplicate/route.ts
+++ app/api/boardrooms/duplicate/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { Boardroom, Prisma } from "@prisma/client";
+import {  Prisma } from "@prisma/client";
 
 export async function POST(request: NextRequest) {
   try {
